1349c5697502e88296c4d72888bf7dc4845b34ac
Laboratorio <laboratorio@udla-ec.int>
2023-10-16 16:05:32 -0500

    Primeras lineas de codigo. Estructura del menu basico.

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int numero;
+int respuesta;
+
+int main(){
+
+    printf("Bienvenido a la calculadora de suma de Gauss\n");
+    printf("Por favor, inserte un numeo entero\n");
+
+
+
+
+}
