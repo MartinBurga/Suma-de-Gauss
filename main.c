7f181df64a0c6e255558db68667658c5b4bf884b
Martjn Burga <martin.burga@udla.edu.ec>
2023-10-16 16:33:22 -0500

    Finalizacion del proceso y calculo de la suma de gauss

--- main.c
+++ main.c
@@ -4,12 +4,17 @@
 int numero;
 int respuesta;
 
-int main(){
+int main(void) {
 
     printf("Bienvenido a la calculadora de suma de Gauss\n");
-    printf("Por favor, inserte un numeo entero\n");
+    printf("Por favor, inserte un número entero\n");
+    scanf("%d", &numero);
+    if (numero > 0) {
+        for (int i = 1; i <= numero; i++) {
+            respuesta = respuesta + i;
+        }
+        printf("El resultado de la suma de Gauss realizada es: %d\n", respuesta);
+    }
 
-
-
-
+    return 0;
 }
