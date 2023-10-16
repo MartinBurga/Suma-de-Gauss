ec31ccc98891782708e6753da3010634c6f91ba9
Martjn Burga <martin.burga@udla.edu.ec>
2023-10-16 16:43:28 -0500

    Finalizacion del algoritmo y un pequeño control ante un numero de caracter no entero o negativo.

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int respuesta;
 int main(void) {
 
     printf("Bienvenido a la calculadora de suma de Gauss\n");
-    printf("Por favor, inserte un número entero\n");
+    printf("Por favor, inserte un numero entero\n");
     scanf("%d", &numero);
     if (numero > 0) {
         for (int i = 1; i <= numero; i++) {
@@ -16,5 +16,11 @@ int main(void) {
         printf("El resultado de la suma de Gauss realizada es: %d\n", respuesta);
     }
 
+    if (numero<0)
+    {
+        printf("El numero insertado no es de caracter entero o entero positivo. Vuelvelo a intentarlo.");
+    }
+    
     return 0;
 }
+
